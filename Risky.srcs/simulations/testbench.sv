881f7da117d430e2e8bfa2fcba4ae325ba8271ef
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-10-18 19:52:50 +0300

    Fix sync and async logic

--- Risky.srcs/simulations/testbench.sv
+++ Risky.srcs/simulations/testbench.sv
@@ -1,15 +1,6 @@
 `include "architecture.vh"
 
 
-`define WAIT_CLK_POS    @ (posedge clk)
-`define WAIT_CLK_NEG    @ (negedge clk)
-
-`define HPERIOD         10
-`define PERIOD          20
-`define DPERIOD         40
-`define QPERIOD         80
-
-
 module testbench;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -19,19 +10,19 @@ module testbench;
 ////////////////////////////////////////////////////////////////////////////////
 
 /* Control signals */
-reg                            clk;
-reg                            rst;
+bit                           clk;
+bit                           rst;
 
 /* Signals related to the program memory */
-reg  [`INSTRUCTION_SIZE - 1:0] instruction;
-wire [`ADDRESS_SIZE - 1:0]     pc;
+bit [`INSTRUCTION_SIZE - 1:0] instruction;
+bit [`ADDRESS_SIZE - 1:0]     pc;
 
 /* Signals related to the data memory */
-wire                           read;
-wire                           write;
-wire [`ADDRESS_SIZE - 1:0]	   address;
-wire [`DATA_SIZE - 1:0]	       data_out;
-reg  [`DATA_SIZE - 1:0]	       data_in;
+bit                           read;
+bit                           write;
+bit [`ADDRESS_SIZE - 1:0]	  address;
+bit [`DATA_SIZE - 1:0]	      data_out;
+bit [`DATA_SIZE - 1:0]	      data_in;
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -61,8 +52,9 @@ golden_core dut(
 
 initial begin
     clk = 0;
-    forever #`HPERIOD clk = ~clk;
+    forever #10 clk = ~clk;
 end
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
@@ -70,97 +62,124 @@ end
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
+task nop();
+    @ (negedge clk);
+endtask
+
+task wait_new_pc();
+    @ (pc);
+endtask;
+
 task reset_core();
+    instruction = `NOP;
     data_in = 0;
     rst = 0;
-    `WAIT_CLK_POS;
-    rst = 1;
+    #1 rst = 1;
 endtask
 
 task load_registers();
     instruction = { `LOADC, `R0, 8'hD }; // R0 = 0x0000_000D
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R1, 8'hE }; // R1 = 0x0000_000E
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R2, 8'hA }; // R2 = 0x0000_000A
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R3, 8'hD }; // R3 = 0x0000_000D
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R4, 8'hB }; // R4 = 0x0000_000B
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R5, 8'hA }; // R5 = 0x0000_000A
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R6, 8'hB }; // R6 = 0x0000_000B
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `LOADC, `R7, 8'hE }; // R7 = 0x0000_000E
-    `WAIT_CLK_NEG;
+    wait_new_pc();
 endtask
 
 task perform_arithmetic_operations();
     instruction = { `ADD, `R0, `R1, `R2 };  // R0 = 0x0000_0018
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `SUB, `R7, `R5, `R6 };  // R7 = 0xFFFF_FFFF
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `ADDF, `R6, `R7, `R3 }; // R6 = 0x0000_000C
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `SUBF, `R1, `R5, `R6 }; // R1 = 0xFFFF_FFFE
-    `WAIT_CLK_NEG;
+    wait_new_pc();
 endtask
 
 task perform_logic_operations();
     instruction = { `AND, `R3, `R1, `R3 };  // R3 = 0x0000_000C
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `OR, `R2, `R6, `R0 };   // R2 = 0x0000_001C
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `XOR, `R4, `R1, `R0 };  // R4 = 0xFFFF_FFE6
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `NAND, `R5, `R2, `R3 }; // R5 = 0xFFFF_FFF3
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `NOR, `R0, `R4, `R6 };  // R0 = 0x0000_0011
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `XNOR, `R3, `R1, `R5 }; // R3 = 0xFFFF_FFF2
-    `WAIT_CLK_NEG;
+    wait_new_pc();
 endtask
 
 task perform_shift_operations();
     instruction = { `SHIFTR, `R4, 6'd3 };   // R4 = 0x1FFF_FFFC
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `SHIFTRA, `R1, 6'd4 };  // R1 = 0x0FFF_FFFF
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `SHIFTL, `R6, 6'd2 };   // R6 = 0x0000_0030
-    `WAIT_CLK_NEG;
+    wait_new_pc();
 endtask
 
 task perform_memory_operations();
     data_in = `DATA_SIZE'hDEAD_BEEF;
     instruction = { `LOAD, `R0, 5'd0, `R6 }; // R0 = M[R6] = M[0x30] = 0xDEAD_BEEF
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     data_in = 0;
     instruction = { `STORE, `R2, 5'd0, `R4}; // M[0x1C] = R4 = 0x1FFF_FFFC
-    `WAIT_CLK_NEG;
+    wait_new_pc();
 endtask
 
 task perform_jump_operations();
     instruction = { `JMP, 9'd0, `R6 };              // PC = 0x030
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `JMPR, 6'd0, 6'd20 };           // PC = 0x044
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `JMPR, 6'd0, -6'd10 };          // PC = 0x03A
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `JMPCOND, `N, `R3, 3'b0, `R2 }; // PC = 0x01C 
-    `WAIT_CLK_NEG;
+    wait_new_pc();
     instruction = { `JMPRCOND, `NN, `R6, 6'd5 };    // PC = 0x021
-    `WAIT_CLK_NEG;
+    wait_new_pc();
 endtask
 
-task perform_nop_and_reset_and_halt();
+task perform_nop_and_halt();
     instruction = `NOP;
-    repeat (10) `WAIT_CLK_NEG;
-    reset_core();
+    repeat (10) nop();
     instruction = `HALT;
-    repeat (10) `WAIT_CLK_NEG;
+    repeat (10) nop();
 endtask;
 
+task execute_small_programme();
+    localparam NUMBER_OF_INSTRS = 9;
+
+    static bit [`INSTRUCTION_SIZE - 1:0] instructions [0:NUMBER_OF_INSTRS - 1] = {
+        { `LOADC, `R0, 8'hD }, // R0 = 0x0000_000D
+        { `LOADC, `R1, 8'hE }, // R1 = 0x0000_000E
+        { `LOADC, `R2, 8'hA }, // R2 = 0x0000_000A
+        { `LOADC, `R3, 8'hD }, // R3 = 0x0000_000D
+        { `LOADC, `R4, 8'hB }, // R4 = 0x0000_000B
+        { `LOADC, `R5, 8'hE }, // R5 = 0x0000_000E
+        { `LOADC, `R6, 8'hE }, // R6 = 0x0000_000E
+        { `LOADC, `R7, 8'hF }, // R7 = 0x0000_000F
+        `HALT
+    };
+  
+    repeat (NUMBER_OF_INSTRS) begin
+        instruction = instructions[pc];
+        wait_new_pc();
+    end
+endtask;
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
@@ -170,17 +189,16 @@ endtask;
 
 initial begin
     reset_core();
-end
-
-initial begin
     load_registers();
     perform_arithmetic_operations();
     perform_logic_operations();
     perform_shift_operations();
     perform_memory_operations();
     perform_jump_operations();
-    perform_nop_and_reset_and_halt();
-    #`HPERIOD $finish;
+    perform_nop_and_halt();
+    reset_core();
+    execute_small_programme();
+    #100 $finish;
 end
 
 
--- Risky.srcs/sources/golden_core.v
+++ Risky.srcs/sources/golden_core.v
@@ -28,13 +28,168 @@ reg [`INST_TYPE_SIZE - 1:0] op_type;
 reg [`GPR_SIZE - 1:0] operand0;
 reg [`GPR_SIZE - 1:0] operand1;
 reg [`GPR_SIZE - 1:0] operand2;
+reg [`DATA_SIZE - 1:0] result;
 reg [`VALUE_SIZE - 1:0] value;
 reg [`CONSTANT_SIZE - 1:0] constant;
 reg [`OFFSET_SIZE - 1:0] offset;
 reg [`CONDITION_SIZE - 1:0] condition;
+reg [`ADDRESS_SIZE - 1:0] next_pc;
 
 
-// Main logic
+// Main asynchronous block
+always @ (*) begin
+
+    // Get the instruction type
+    op_type = instruction[`INST_SELECT];
+
+    // Decode the intruction
+    case (op_type)
+        `ARITHMETIC : begin
+            operand0 <= instruction[8:6];
+            operand1 <= instruction[5:3];
+            operand2 <= instruction[2:0];
+        end
+        
+        `LOGIC : begin
+            operand0 <= instruction[8:6];
+            operand1 <= instruction[5:3];
+            operand2 <= instruction[2:0];
+        end
+        
+        `SHIFT : begin
+            operand0 <= instruction[8:6];
+            value    <= instruction[5:0];
+        end
+        
+        `MEMORY_ACCESS : begin
+            operand0 <= instruction[10:8];
+            operand1 <= instruction[2:0];
+            constant <= instruction[7:0];
+        end
+        
+        `JUMP : begin
+            operand0 <= instruction[2:0];
+            offset   <= instruction[5:0];
+        end
+        
+        `JUMP_COND : begin
+            condition <= instruction[11:9];
+            operand0  <= instruction[8:6];
+            operand1  <= instruction[2:0];
+            offset    <= instruction[5:0];
+        end
+    endcase
+    
+    // Compute the result of the instruction that interacts with registers and/or data memory
+    case (op_type)
+        `ARITHMETIC : begin
+            case (instruction[`ARITHMETIC_SELECT])
+                `ADD  : result <= registers[operand1] + registers[operand2];
+                `ADDF : result <= registers[operand1] + registers[operand2];
+                `SUB  : result <= registers[operand1] - registers[operand2];
+                `SUBF : result <= registers[operand1] - registers[operand2];
+            endcase
+        end
+        
+        `LOGIC : begin
+            case (instruction[`LOGIC_SELECT])
+                `AND  : result <=  ( registers[operand1] & registers[operand2] );
+                `OR   : result <=  ( registers[operand1] | registers[operand2] );
+                `XOR  : result <=  ( registers[operand1] ^ registers[operand2] );
+                `NAND : result <= ~( registers[operand1] & registers[operand2] );
+                `NOR  : result <= ~( registers[operand1] | registers[operand2] );
+                `XNOR : result <= ~( registers[operand1] ^ registers[operand2] );
+            endcase
+        end
+        
+        `SHIFT : begin
+            case (instruction[`SHIFT_SELECT])
+                `SHIFTR  : result <= registers[operand0] >>  value;
+                `SHIFTRA : result <= registers[operand0] >>> value;
+                `SHIFTL  : result <= registers[operand0] <<  value;
+            endcase
+        end
+        
+        `MEMORY_ACCESS : begin
+            case (instruction[`MEMORY_ACCESS_SELECT])
+                `LOAD : begin
+                    result <= data_in;
+                end
+                
+                `LOADC : begin
+                    result <= { registers[operand0][`DATA_SIZE - 1:8], constant };
+                end
+            endcase
+        end
+        
+        default : result <= 0;
+    endcase
+    
+    // Process instructions related to data memory
+    if (instruction[`MEMORY_ACCESS_SELECT] == `LOAD) begin
+        address <= registers[operand1][`ADDRESS_SIZE - 1:0];
+        read    <= 1'b1;
+    end
+    else begin
+        address <= `ADDRESS_SIZE'b0;
+        read    <= 1'b0;
+    end
+    
+    if (instruction[`MEMORY_ACCESS_SELECT] == `STORE) begin
+        address  <= registers[operand0][`ADDRESS_SIZE - 1:0];
+        data_out <= registers[operand1];
+        write    <= 1'b1;
+    end
+    else begin
+        address  <= `ADDRESS_SIZE'b0;
+        data_out <= `DATA_SIZE'b0;
+        write    <= 1'b0;
+    end
+    
+    // Compute the next Program Counter based on the instruction received
+    case (op_type)
+        `SPECIAL : begin
+            case (instruction)
+                `NOP  : next_pc <= pc + 1;
+                `HALT : next_pc <= pc;
+            endcase
+        end
+        
+        `JUMP : begin
+            case (instruction[`JUMP_SELECT])
+                `JMP  : next_pc <= registers[operand0][`ADDRESS_SIZE - 1:0];
+                `JMPR : next_pc <= pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset });
+            endcase
+        end
+
+        `JUMP_COND : begin
+            case (instruction[`JUMP_SELECT])
+                `JMPCOND : begin
+                    case (condition)
+                        `N  : next_pc <= $signed(registers[operand0]) <  0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1; 
+                        `NN : next_pc <= $signed(registers[operand0]) >= 0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1;
+                        `Z  : next_pc <= $signed(registers[operand0]) == 0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1;
+                        `NZ : next_pc <= $signed(registers[operand0]) != 0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1;
+                    endcase
+                end
+                
+                `JMPRCOND : begin
+                    case (condition)
+                        `N  : next_pc <= $signed(registers[operand0])  < 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
+                        `NN : next_pc <= $signed(registers[operand0]) >= 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
+                        `Z  : next_pc <= $signed(registers[operand0]) == 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
+                        `NZ : next_pc <= $signed(registers[operand0]) != 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
+                    endcase
+                end
+            endcase
+        end
+
+        default : next_pc <= pc + 1;
+    endcase
+end
+
+
+// Main synchronous block
 always @ (posedge clk or negedge rst) begin
 
     // Treat the Reset signal
@@ -42,8 +197,8 @@ always @ (posedge clk or negedge rst) begin
         pc <= 0;
         read <= 0;
         write <= 0;
-        data_out <= 0;
         address <= 0;
+        data_out <= 0;
         
         registers[`R0] <= 0;
         registers[`R1] <= 0;
@@ -56,153 +211,35 @@ always @ (posedge clk or negedge rst) begin
     end
     
     else begin
-        // Get the instruction type
-        op_type = instruction[`INST_SELECT];
-    
-        // Decode the intruction
-        case (op_type)
-            `ARITHMETIC : begin
-                operand0 = instruction[8:6];
-                operand1 = instruction[5:3];
-                operand2 = instruction[2:0];
-            end
-            
-            `LOGIC : begin
-                operand0 = instruction[8:6];
-                operand1 = instruction[5:3];
-                operand2 = instruction[2:0];
-            end
-            
-            `SHIFT : begin
-                operand0 = instruction[8:6];
-                value    = instruction[5:0];
-            end
-            
-            `MEMORY_ACCESS : begin
-                operand0 = instruction[10:8];
-                operand1 = instruction[2:0];
-                constant = instruction[7:0];
-            end
-            
-            `JUMP : begin
-                operand0 = instruction[2:0];
-                offset   = instruction[5:0];
-            end
-            
-            `JUMP_COND : begin
-                condition = instruction[11:9];
-                operand0  = instruction[8:6];
-                operand1  = instruction[2:0];
-                offset    = instruction[5:0];
-            end
-        endcase
-        
         // Execute the instruction that interacts with registers and/or data memory
         case (op_type)
             `ARITHMETIC : begin
-                case (instruction[`ARITHMETIC_SELECT])
-                    `ADD  : registers[operand0] = registers[operand1] + registers[operand2];
-                    `ADDF : registers[operand0] = registers[operand1] + registers[operand2];
-                    `SUB  : registers[operand0] = registers[operand1] - registers[operand2];
-                    `SUBF : registers[operand0] = registers[operand1] - registers[operand2];
-                endcase
+                registers[operand0] <= result;
             end
             
-            `LOGIC : begin
-                case (instruction[`LOGIC_SELECT])
-                    `AND  : registers[operand0] = registers[operand1] & registers[operand2];
-                    `OR   : registers[operand0] = registers[operand1] | registers[operand2];
-                    `XOR  : registers[operand0] = registers[operand1] ^ registers[operand2];
-                    `NAND : registers[operand0] = ~( registers[operand1] & registers[operand2] );
-                    `NOR  : registers[operand0] = ~( registers[operand1] | registers[operand2] );
-                    `XNOR : registers[operand0] = ~( registers[operand1] ^ registers[operand2] );
-                endcase
+            `LOGIC : begin 
+                registers[operand0] <= result;
             end
             
             `SHIFT : begin
-                case (instruction[`SHIFT_SELECT])
-                    `SHIFTR  : registers[operand0] = registers[operand0] >> value;
-                    `SHIFTRA : registers[operand0] = registers[operand0] >>> value;
-                    `SHIFTL  : registers[operand0] = registers[operand0] << value;
-                endcase
+                registers[operand0] <= result;
             end
             
             `MEMORY_ACCESS : begin
                 case (instruction[`MEMORY_ACCESS_SELECT])
                     `LOAD : begin
-                        address = registers[operand1][`ADDRESS_SIZE - 1:0];
-                        registers[operand0] = data_in;
-                        read = 1;
+                        registers[operand0] <= result;
                     end
                     
                     `LOADC : begin
-                        registers[operand0] = { registers[operand0][`DATA_SIZE - 1:8], constant };
-                    end
-                    
-                    `STORE : begin
-                        address = registers[operand0][`ADDRESS_SIZE - 1:0];
-                        data_out = registers[operand1];
-                        write = 1;
+                        registers[operand0] <= result;
                     end
                 endcase
             end
         endcase
-        
-        // Read and Write signals for data memory
-        if (instruction[`MEMORY_ACCESS_SELECT] == `LOAD) begin
-            read = 1;
-        end
-        else begin
-            read = 0;
-        end
-        
-        if (instruction[`MEMORY_ACCESS_SELECT] == `STORE) begin
-            write = 1;
-        end
-        else begin
-            write = 0;
-        end
 
-        // Update the Program Counter based on the instruction received
-        case (op_type)
-            `SPECIAL : begin
-                case (instruction)
-                    `NOP  : pc = pc + 1;
-                    `HALT : pc = pc;
-                endcase
-            end
-            
-            `JUMP : begin
-                case (instruction[`JUMP_SELECT])
-                    `JMP  : pc = registers[operand0][`ADDRESS_SIZE - 1:0];
-                    `JMPR : pc = pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset });
-                endcase
-            end
-
-            `JUMP_COND : begin
-                case (instruction[`JUMP_SELECT])
-                    `JMPCOND : begin
-                        case (condition)
-                            `N  : pc = $signed(registers[operand0]) < 0  ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1; 
-                            `NN : pc = $signed(registers[operand0]) >= 0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1;
-                            `Z  : pc = $signed(registers[operand0]) == 0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1;
-                            `NZ : pc = $signed(registers[operand0]) != 0 ? registers[operand1][`ADDRESS_SIZE - 1:0] : pc + 1;
-                        endcase
-                    end
-                    
-                    `JMPRCOND : begin
-                        case (condition)
-                            `N  : pc = $signed(registers[operand0]) < 0  ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
-                            `NN : pc = $signed(registers[operand0]) >= 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
-                            `Z  : pc = $signed(registers[operand0]) == 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
-                            `NZ : pc = $signed(registers[operand0]) != 0 ? pc + $signed({ { `ADDRESS_SIZE - `OFFSET_SIZE{ offset[`OFFSET_SIZE - 1] } }, offset }) : pc + 1;
-                        endcase
-                    end
-                endcase
-            end
-
-            default : pc = pc + 1;
-        endcase
+        // Update the Program Counter
+        pc <= next_pc;
     end
 end
 
