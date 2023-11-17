20aeda254c8b8daa83f64007d8761e006bf46c25
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-11-17 15:51:14 +0200

    Fix combinational logic

--- Risky/rtl/fetch_unit.v
+++ Risky/rtl/fetch_unit.v
@@ -18,30 +18,44 @@ module fetch_unit
 );
 
 
-always @ (posedge clock or negedge reset) begin
-    // Reset
-    if (!reset) begin                     
-        pc              <= 0;
-        last_pc         <= 0;
-        instruction_out <= `NOP_INST;
-    end
+reg [`ADDRESS_SIZE - 1:0]     int_pc;
+reg [`ADDRESS_SIZE - 1:0]     int_last_pc;
+reg [`INSTRUCTION_SIZE - 1:0] int_instruction_out;
+
+
+// Combinationally fetch the instruction and set the program counter
+always @ (*) begin
     // Stall or halt
-    else if (halt || stall) begin
-        pc              <= pc;
-        last_pc         <= last_pc;
-        instruction_out <= instruction_out;
+    if (halt || stall) begin
+        int_pc              = pc;
+        int_last_pc         = last_pc;
+        int_instruction_out = instruction_out;
     end
     // Jump
     else if (jump) begin
-        pc              <= jump_pc;
-        last_pc         <= jump_pc;
-        instruction_out <= `NOP_INST;
+        int_pc              = jump_pc;
+        int_last_pc         = jump_pc;
+        int_instruction_out = `NOP_INST;
     end
     // Normal operation
     else begin
-        pc              <= pc + 1;
-        last_pc         <= pc;
-        instruction_out <= instruction;
+        int_pc              = pc + 1;
+        int_last_pc         = pc;
+        int_instruction_out = instruction;
+    end
+end
+
+
+always @ (posedge clock or negedge reset) begin
+    if (!reset) begin                     
+        pc              <= `ADDRESS_SIZE'b0;
+        last_pc         <= `ADDRESS_SIZE'b0;
+        instruction_out <= `NOP_INST;
+    end
+    else begin
+        pc              <= int_pc;
+        last_pc         <= int_last_pc;
+        instruction_out <= int_instruction_out;
     end
 end
 
--- Risky/rtl/read_unit.v
+++ Risky/rtl/read_unit.v
@@ -31,11 +31,22 @@ module read_unit
 
 reg [`INST_TYPE_SIZE - 1:0] inst_type;
 
+reg [`OPCODE_SIZE - 1:0]    int_opcode;
+reg [`DATA_SIZE - 1:0]      int_operand0;
+reg [`DATA_SIZE - 1:0]      int_operand1;
+reg [`DATA_SIZE - 1:0]      int_operand2;
+reg [`VALUE_SIZE - 1:0]     int_value;
+reg [`CONSTANT_SIZE - 1:0]  int_constant;
+reg [`OFFSET_SIZE - 1:0]    int_offset;
+reg [`CONDITION_SIZE - 1:0] int_condition;
+reg [`ADDRESS_SIZE - 1:0]   int_read_pc;
 
-// Combinationally decode the instruction and set the register file read addresses
+
+// Combinationally decode the instruction and set the register file read addresses and pipeline registers
 always @ (*) begin
     inst_type = instruction[`INST_SELECT];
 
+    // Combinationally set the register file read addresses
     case (inst_type)
         `ARITHMETIC, `LOGIC : begin
             read_address0 = instruction[5:3];
@@ -104,84 +115,78 @@ always @ (*) begin
             read_address1 = `GPR_SIZE'b0;
         end
     endcase
-end
 
-// Synchronous decode the instruction and set the pipeline second stage output
-always @ (posedge clock or negedge reset) begin
-    if (!reset) begin
-        opcode    <= `NOP;
-        operand0  <= `DATA_SIZE'b0;
-        operand1  <= `DATA_SIZE'b0;
-        operand2  <= `DATA_SIZE'b0;
-        value     <= `VALUE_SIZE'b0;
-        constant  <= `CONSTANT_SIZE'b0;
-        offset    <= `OFFSET_SIZE'b0;
-        condition <= `CONDITION_SIZE'b0;
-        read_pc   <= `ADDRESS_SIZE'b0;
-    end
-    else if (stall || jump) begin
-        opcode <= `NOP;
+    // Combinationally set the pipeline registers
+    if (stall || jump) begin
+        int_opcode    = `NOP;
+        int_read_pc   = `ADDRESS_SIZE'b0;
+        int_operand0  = `DATA_SIZE'b0;
+        int_operand1  = `DATA_SIZE'b0;
+        int_operand2  = `DATA_SIZE'b0;
+        int_value     = `VALUE_SIZE'b0;
+        int_constant  = `CONSTANT_SIZE'b0;
+        int_offset    = `OFFSET_SIZE'b0;
+        int_condition = `CONDITION_SIZE'b0;
     end
     else begin
-        read_pc <= pc;
-
-        opcode <= instruction[`OPCODE_SELECT];
+        int_read_pc = pc;
+        int_opcode  = instruction[`OPCODE_SELECT];
 
         case (inst_type)
             `ARITHMETIC, `LOGIC : begin
-                operand0 <= instruction[8:6];
-                operand1 <= read_data0;
-                operand2 <= read_data1;
+                int_operand0 = instruction[8:6];
+                int_operand1 = read_data0;
+                int_operand2 = read_data1;
 
-                value     <= `VALUE_SIZE'b0;
-                constant  <= `CONSTANT_SIZE'b0;
-                offset    <= `OFFSET_SIZE'b0;
-                condition <= `CONDITION_SIZE'b0;
+                int_value     = `VALUE_SIZE'b0;
+                int_constant  = `CONSTANT_SIZE'b0;
+                int_offset    = `OFFSET_SIZE'b0;
+                int_condition = `CONDITION_SIZE'b0;
             end
 
             `SHIFT : begin
-                operand0 <= instruction[8:6];
-                operand1 <= read_data0;
-                value    <= instruction[5:0];
+                int_operand0 = instruction[8:6];
+                int_operand1 = read_data0;
+                int_value    = instruction[5:0];
 
-                operand2  <= `DATA_SIZE'b0;
-                constant  <= `CONSTANT_SIZE'b0;
-                offset    <= `OFFSET_SIZE'b0;
-                condition <= `CONDITION_SIZE'b0;
+                int_operand2  = `DATA_SIZE'b0;
+                int_constant  = `CONSTANT_SIZE'b0;
+                int_offset    = `OFFSET_SIZE'b0;
+                int_condition = `CONDITION_SIZE'b0;
             end
 
             `MEMORY_ACCESS : begin
                 case (instruction[`MEMORY_ACCESS_SELECT])
                     `LOAD, `STORE : begin
-                        operand0 <= instruction[10:8];
-                        operand1 <= read_data0;
+                        int_operand0 = instruction[10:8];
+                        int_operand1 = read_data0;
 
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        constant  <= `CONSTANT_SIZE'b0;
-                        offset    <= `OFFSET_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_constant  = `CONSTANT_SIZE'b0;
+                        int_offset    = `OFFSET_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
 
                     `LOADC : begin
-                        operand0 <= instruction[10:8];
-                        constant <= instruction[7:0];
+                        int_operand0 = instruction[10:8];
+                        int_constant = instruction[7:0];
 
-                        operand1  <= `DATA_SIZE'b0;
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        offset    <= `OFFSET_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand1  = `DATA_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_offset    = `OFFSET_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
                     
                     default : begin
-                        operand0  <= `DATA_SIZE'b0;
-                        operand1  <= `DATA_SIZE'b0;
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        constant  <= `CONSTANT_SIZE'b0;
-                        offset    <= `OFFSET_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand0  = `DATA_SIZE'b0;
+                        int_operand1  = `DATA_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_constant  = `CONSTANT_SIZE'b0;
+                        int_offset    = `OFFSET_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
                 endcase
             end
@@ -189,35 +194,35 @@ always @ (posedge clock or negedge reset) begin
             `JUMP : begin
                 case (instruction[`JUMP_SELECT])
                     `JMP  : begin
-                        operand0 <= read_data0;
+                        int_operand0 = read_data0;
 
-                        operand1  <= `DATA_SIZE'b0;
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        constant  <= `CONSTANT_SIZE'b0;
-                        offset    <= `OFFSET_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand1  = `DATA_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_constant  = `CONSTANT_SIZE'b0;
+                        int_offset    = `OFFSET_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
 
                     `JMPR : begin
-                        offset <= instruction[5:0];
+                        int_offset = instruction[5:0];
 
-                        operand0  <= `DATA_SIZE'b0;
-                        operand1  <= `DATA_SIZE'b0;
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        constant  <= `CONSTANT_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand0  = `DATA_SIZE'b0;
+                        int_operand1  = `DATA_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_constant  = `CONSTANT_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
 
                     default : begin
-                        operand0  <= `DATA_SIZE'b0;
-                        operand1  <= `DATA_SIZE'b0;
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        constant  <= `CONSTANT_SIZE'b0;
-                        offset    <= `OFFSET_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand0  = `DATA_SIZE'b0;
+                        int_operand1  = `DATA_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_constant  = `CONSTANT_SIZE'b0;
+                        int_offset    = `OFFSET_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
                 endcase
             end
@@ -225,51 +230,78 @@ always @ (posedge clock or negedge reset) begin
             `JUMP_COND : begin
                 case (instruction[`JUMP_SELECT])
                     `JMPCOND : begin
-                        operand0  <= read_data0;
-                        operand1  <= read_data1;
-                        condition <= instruction[11:9];
+                        int_operand0  = read_data0;
+                        int_operand1  = read_data1;
+                        int_condition = instruction[11:9];
 
-                        operand2 <= `DATA_SIZE'b0;
-                        value    <= `VALUE_SIZE'b0;
-                        constant <= `CONSTANT_SIZE'b0;
-                        offset   <= `OFFSET_SIZE'b0;
+                        int_operand2 = `DATA_SIZE'b0;
+                        int_value    = `VALUE_SIZE'b0;
+                        int_constant = `CONSTANT_SIZE'b0;
+                        int_offset   = `OFFSET_SIZE'b0;
                     end
 
                     `JMPRCOND : begin
-                        operand0  <= read_data0;
-                        offset    <= instruction[5:0];
-                        condition <= instruction[11:9];
+                        int_operand0  = read_data0;
+                        int_offset    = instruction[5:0];
+                        int_condition = instruction[11:9];
 
-                        operand1 <= `DATA_SIZE'b0;
-                        operand2 <= `DATA_SIZE'b0;
-                        value    <= `VALUE_SIZE'b0;
-                        constant <= `CONSTANT_SIZE'b0;
+                        int_operand1 = `DATA_SIZE'b0;
+                        int_operand2 = `DATA_SIZE'b0;
+                        int_value    = `VALUE_SIZE'b0;
+                        int_constant = `CONSTANT_SIZE'b0;
                     end
                     
                     default : begin
-                        operand0  <= `DATA_SIZE'b0;
-                        operand1  <= `DATA_SIZE'b0;
-                        operand2  <= `DATA_SIZE'b0;
-                        value     <= `VALUE_SIZE'b0;
-                        constant  <= `CONSTANT_SIZE'b0;
-                        offset    <= `OFFSET_SIZE'b0;
-                        condition <= `CONDITION_SIZE'b0;
+                        int_operand0  = `DATA_SIZE'b0;
+                        int_operand1  = `DATA_SIZE'b0;
+                        int_operand2  = `DATA_SIZE'b0;
+                        int_value     = `VALUE_SIZE'b0;
+                        int_constant  = `CONSTANT_SIZE'b0;
+                        int_offset    = `OFFSET_SIZE'b0;
+                        int_condition = `CONDITION_SIZE'b0;
                     end
                 endcase
             end
             
             default : begin
-                opcode    <= `NOP;
-                operand0  <= `DATA_SIZE'b0;
-                operand1  <= `DATA_SIZE'b0;
-                operand2  <= `DATA_SIZE'b0;
-                value     <= `VALUE_SIZE'b0;
-                constant  <= `CONSTANT_SIZE'b0;
-                offset    <= `OFFSET_SIZE'b0;
-                condition <= `CONDITION_SIZE'b0;
-                read_pc   <= `ADDRESS_SIZE'b0;
+                int_opcode    = `NOP;
+                int_operand0  = `DATA_SIZE'b0;
+                int_read_pc   = `ADDRESS_SIZE'b0;
+                int_operand1  = `DATA_SIZE'b0;
+                int_operand2  = `DATA_SIZE'b0;
+                int_value     = `VALUE_SIZE'b0;
+                int_constant  = `CONSTANT_SIZE'b0;
+                int_offset    = `OFFSET_SIZE'b0;
+                int_condition = `CONDITION_SIZE'b0;
+
             end
         endcase
+    end
+end
+
+
+always @ (posedge clock or negedge reset) begin
+    if (!reset) begin
+        opcode    <= `NOP;
+        read_pc   <= `ADDRESS_SIZE'b0;
+        operand0  <= `DATA_SIZE'b0;
+        operand1  <= `DATA_SIZE'b0;
+        operand2  <= `DATA_SIZE'b0;
+        value     <= `VALUE_SIZE'b0;
+        constant  <= `CONSTANT_SIZE'b0;
+        offset    <= `OFFSET_SIZE'b0;
+        condition <= `CONDITION_SIZE'b0;
+    end
+    else begin
+        opcode    <= int_opcode;
+        read_pc   <= int_read_pc;
+        operand0  <= int_operand0;
+        operand1  <= int_operand1;
+        operand2  <= int_operand2;
+        value     <= int_value;
+        constant  <= int_constant;
+        offset    <= int_offset;
+        condition <= int_condition;
     end
 end
 
